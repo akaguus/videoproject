50dfe7fce10f158296ee8ba2a7ab31a7520f29a4
akaguus <akaguus@gmail.com>
2014-07-06 06:11:51 -0500

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Videoplatform::Application.config.secret_token = '88cd81de33fccc39881742c0bfca5c89eb52934ccaf83ebedd86ed410bc1acff6da3cc6dbe6ad5cd354f4ad7ad98fa0a0d509f86728437d1fa0b7f8d411b98ca'
